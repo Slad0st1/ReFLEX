81ed48af96e9c593a353cc208529e794d782595f
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-07-05 22:57:06 +0300

    fix issue

--- src/components/Header/Cart/SideBar/SideBarItem/SideBarItem.jsx
+++ src/components/Header/Cart/SideBar/SideBarItem/SideBarItem.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import styles from "./SideBarItem.module.css";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeItem } from "../../../../../store/Items/ItemsSlice";
 
-export default function SideBarItem({id , categori ,title, price, img  }) {
-
+export default function SideBarItem(target) {
+  const { id, categori, title, price, img } = target;
+  const dispatch = useDispatch();
   return (
     <div className={styles.card}>
-      <NavLink style={{color : 'black' , textDecoration : 'none'}} to={`/${categori}/${id}`}>
-      <div className={styles.cardImg}>
-        <img src={img} />
-      </div>
-      <div className={styles.text}>
-        <div className={styles.title}>{title}</div>
-      </div>
+      <NavLink
+        style={{ color: "black", textDecoration: "none" }}
+        to={`/${categori}/${id}`}
+      >
+        <div className={styles.cardImg}>
+          <img src={img} />
+        </div>
+        <div className={styles.text}>
+          <div className={styles.title}>{title}</div>
+        </div>
       </NavLink>
       <div className={styles.bottom}>
-          <div className={styles.price}>{price}$</div>
-          <img  src='/close.svg' alt='Close'/>
-        </div>
+        <div className={styles.price}>{price}$</div>
+        <img
+          onClick={() => dispatch(removeItem(target))}
+          src="/close.svg"
+          alt="Close"
+        />
+      </div>
     </div>
   );
 }
