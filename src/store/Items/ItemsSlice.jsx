30c505aa82cbb5f3f62d1f625ac5b64f72645673
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-07-05 22:45:45 +0300

    update hardcoded to mock

--- src/store/Items/ItemsSlice.jsx
+++ src/store/Items/ItemsSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { T_SHIRTS_MOCKUP, SHOES_MOCK, PANTS_MOCK } from "../../__mock/tshirts";
 
 const Items = createSlice({
   name: "items",
@@ -8,156 +9,9 @@ const Items = createSlice({
       { id: 2, name: "shoes", title: "Shoes" },
       { id: 3, name: "pants", title: "Pants" },
     ],
-    tshirts: [
-      {
-        id:  1,
-        price: 1,
-        title: "GUCCI 2022G Summer Brand",
-        img: "/tshirt.jpg",
-        type: "tshirts",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: false,
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-      {
-        id: 2,
-        price: 2,
-        title: "GUCCI 2022G Summer",
-        img: "/tshirt.jpg",
-        type: "tshirts",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: false,
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-      {
-        id: 3,
-        price: 3,
-        title: "GUCCI 2022G Brand",
-        img: "/tshirt.jpg",
-        type: "tshirts",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: false,
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-    ],
-    shoes: [
-      {
-        id: 1,
-        price: 1,
-        title: "Shoes GUCCI 2022G Summer Brand",
-        img: "/shoes.jpg",
-        type: "shoes",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: "false",
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-      {
-        id: 2,
-        price: 2,
-        title: "Shoes GUCCI 2022G Summer ",
-        img: "/shoes.jpg",
-        type: "shoes",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: "false",
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-      {
-        id: 3,
-        price: 3,
-        title: "Shoes GUCCI 2022G Brand",
-        img: "/shoes.jpg",
-        type: "shoes",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: "false",
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-    ],
-    pants: [
-      {
-        id: 1,
-        price: 1,
-        title: "Pants GUCCI 2022G Summer Brand",
-        img: "/pants.jpg",
-        type: "pants",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: "false",
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-      {
-        id: 2,
-        price: 2,
-        title: "Pants GUCCI 2022G Summer ",
-        img: "/pants.jpg",
-        type: "pants",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: "false",
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-      {
-        id: 3,
-        price: 3,
-        title: "Pants GUCCI 2022G Brand",
-        img: "/pants.jpg",
-        type: "pants",
-        gallery: [
-          { img: 1, link: "/tshirt1.jpg" },
-          { img: 2, link: "/tshirt2.jpg" },
-        ],
-        sizes: ["L", "XL", "XXL"],
-        color: "Black",
-        added: "false",
-        descr:
-          "Sneaker in air mesh and suede goatskin. Light sole with contrasting design for a versatile and modern look.",
-      },
-    ],
+    tshirts: T_SHIRTS_MOCKUP,
+    shoes: SHOES_MOCK,
+    pants: PANTS_MOCK,
     activeItem: {},
     cart: [],
   },
@@ -172,7 +26,9 @@ const Items = createSlice({
       state.cart.push(action.payload);
     },
     removeItem(state, action) {
-      state.cart = state.cart.filter((i) =>( i.id && i.title) !== (action.payload.id && action.payload.title))
+      state.cart = state.cart.filter(
+        (i) => (i.id && i.title) !== (action.payload.id && action.payload.title)
+      );
     },
   },
 });
